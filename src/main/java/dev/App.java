f9e1490d3d53233ca885cfce16cf676de0e99356
N89e <nunodafonseca@live.fr>
2025-07-25 17:15:17 +0200

    ajout jfiglet

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,10 +1,15 @@
 package dev;
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        String asciiArtTitle = FigletFont.convertOneLine("Mon Titre");
+        System.out.println(asciiArtTitle);
     }
 }
+

a71942370504d01c195ed27cb785ca1ededc4a9b
N89e <nunodafonseca@live.fr>
2025-07-30 11:42:32 +0200

    TP4

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -2,14 +2,27 @@ package dev;
 import com.github.lalyos.jfiglet.FigletFont;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) throws IOException {
+        System.out.println("_______________________________________");
+        System.out.println("----------------TP1--------------------");
         String asciiArtTitle = FigletFont.convertOneLine("Mon Titre");
         System.out.println(asciiArtTitle);
+        System.out.println("_______________________________________");
+        System.out.println("----------------TP2--------------------");
+        String titre = ResourceBundle.getBundle("app").getString("titre");
+        String asciiArt = FigletFont.convertOneLine(titre);
+        System.out.println(asciiArt);
+        System.out.println("_______________________________________");
+        System.out.println("-------------TP3-et-TP4----------------");
+        String environnement = ResourceBundle.getBundle("app").getString("environment");
+        System.out.println("Environnement : " + environnement);
+        System.out.println("_______________________________________");
     }
 }
 
